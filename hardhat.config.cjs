a82d563514966ccb95b25caa9cc5e19f344a54c4
miguelmendez1208 <miguelmendez1208@gmail.com>
2023-07-16 22:07:39 -0500

    hello

---
+++ hardhat.config.cjs
@@ -0,0 +1,11 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+  paths: {
+    artifacts: './src/artifacts',
+  },
+};
